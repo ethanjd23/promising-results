e6c818652738449a2678345abf618ce112f60f0a
ethanjd23 <ethanjd23@gmail.com>
2021-02-23 10:58:57 -0600

    added professional error message

--- app.js
+++ app.js
@@ -20,7 +20,7 @@
 //     console.log(value);
 //     return slowMath.remainder(value, 40);
 // }).then((value) => {
-//     console.log(value);
+//     console.log(value);1
 //     return slowMath.add(value, 32);
 // }).then((value) => {
 //     console.log(`The final result is ${value}.`)
@@ -33,7 +33,7 @@ async function doMath() {
         let value = await slowMath.add(6, 2);
         console.log(value);
         let valueNext = await slowMath.multiply(value, 2);
-        console.log(valueNext);
+        console.log(valueext); // Error is here, needs to be valueNext. 
         let value2 = await slowMath.divide(valueNext, 4);
         console.log(value2);
         let value3 = await slowMath.subtract(value2, 3);
@@ -51,5 +51,10 @@ async function doMath() {
         
     } catch (error) {
         console.error("oh nooo")
+        let video = document.createElement("video");
+        video.src = "error-vid.mp4"
+        video.autoplay = true;
+        video.loop = true;
+        document.body.appendChild(video);
     }}
 doMath();
